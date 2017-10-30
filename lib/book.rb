2e285109cae364f839822da27048d8bf5208f04f
Joshua Needham <joshuabneedham@gmail.com>
2017-10-30 17:05:50 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -6,11 +6,11 @@ class Book
   def initialize(title)
     @title = title
   end
-  
+
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-  
+
   def genre=(genre)
     @genre = genre
     GENRES << genre
